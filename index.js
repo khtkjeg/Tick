48744672c40f246da75ba71c074977842ceda83f
khtkjeg <1151651460@qq.com>
2017-08-01 22:27:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-Ticts.createTicts("dingshiqi1","2018-07-12 21:20:20");
-Ticts.createTicts("dingshiqi2","2018-07-12 21:30:12");
+Ticts.createTicts("dingshiqi1","2018-08-12 21:20:20");
+Ticts.createTicts("dingshiqi2","2018-08-12 21:30:12");
